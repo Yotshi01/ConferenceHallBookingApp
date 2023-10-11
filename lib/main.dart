02f02fd9429d23142ee8b1ba8e55f5c349880fb7
Raghav Joshi <raghav03032002joshi@gmail.com>
2023-10-11 16:01:44 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
