2c6721ffdc0634fd98bb55479ac0212469640777
Yotshi01 <96737993+Yotshi01@users.noreply.github.com>
2023-11-17 13:11:26 +0530

    firebase token generated and stored in database.

---
+++ lib/utils/firebase_token.dart
@@ -0,0 +1,26 @@
+import 'package:conference_hall_booking/source/exported_packages_for_easy_imports.dart';
+import 'package:conference_hall_booking/source/constants.dart';
+
+class PushNotificationService {
+  FirebaseMessaging _fcm = FirebaseMessaging.instance;
+
+  Future initialize() async {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+    // Get the token
+    await getToken();
+  }
+
+  Future<String?> getToken() async {
+    String? token = await _fcm.getToken();
+    print('Ashok Token: $token');
+    currentUserNotificationFirebaseToken = token;
+    return token;
+  }
+}
